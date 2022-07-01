c450ea8b8183c7b0516245247fe3541d81189b65
Ângelo Hideki Noda <angelo.noda@insidesistemas.com.br>
2022-06-30 21:44:16 -0300

    Finalizado front

--- front/src/app/layout/App.tsx
+++ front/src/app/layout/App.tsx
@@ -1,39 +1,31 @@
-import { Container, CssBaseline} from "@mui/material";
-import { useEffect, useState } from "react";
+import { Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import { useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
-import { Product } from "../models/product";
 import Header from "./Header";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? 'dark' : 'light'
+  const theme = createTheme({
+    palette:{
+      mode: palleteType,
+      background:{
+        default: palleteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
 
-  useEffect(() => {
-    fetch('https://localhost:7106/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  }, [])
-  function addProduct() {
-    //setProducts([...products, {name: 'product3', price: 300.00 }])
-    setProducts(prevState => [...prevState, 
-      {
-        id: prevState.length + 101,
-        name: 'product' + (prevState.length + 1),
-        price: (prevState.length *100)+100,
-        brand: 'some brand',
-        description: 'some description',
-        pictureUrl: 'http://picsum.photos/200'
-      }])
-
+  function handleThemeChange(){
+    setDarkMode(!darkMode);
   }
   return (
-    <>
-    <CssBaseline />
-      <Header />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-        <Catalog products={products} addProduct={addProduct}/>
+        <Catalog />
       </Container>
-      
-    </>
+    </ThemeProvider>
   );
 }
 
--- front/src/features/catalog/ProductCard.tsx
+++ front/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { ListItem, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
+import { ListItem, ListItemAvatar, Avatar, ListItemText, Button, Card, CardActions, CardContent, CardMedia, Typography, CardHeader } from "@mui/material";
 import { Product } from "../../app/models/product";
 
 interface Props{
@@ -6,13 +6,38 @@ interface Props{
 }
 export default function ProductCard({product}:Props){
     return(
-        <ListItem key={product.id}>
-            <ListItemAvatar>
-                <Avatar src={product.pictureUrl}/>
-            </ListItemAvatar>
-            <ListItemText>
-                {product.name} - {product.price}
-            </ListItemText>
-        </ListItem>
+          <Card>
+            <CardHeader 
+              avatar={
+                <Avatar sx={{bgcolor: 'secondary.main'}}>
+                  {product.name.charAt(0).toUpperCase()}
+                </Avatar>
+              }
+              title={product.name}
+              titleTypographyProps={{
+                sx: {fontWeight: 'bold', color: 'primary.main'}
+              }}
+            />
+            <CardMedia
+              component="img"
+              sx={{heigth: 140, backgroundSize: 'contain', bgcolor: 'primary.light'}}
+              height="140"
+              //image="http://picsum.photos/200"
+              image={product.pictureUrl}
+              title={product.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" color='secondary'>
+              $ {(product.price / 100).toFixed(2)}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {product.brand} / {product.type}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Add Cart</Button>
+            <Button size="small">View</Button>
+          </CardActions>
+        </Card>
     )
 }
